40e22a4b7dceca3de4fa45bcf0eb848f1e11c179
_iz_ <62268447+izzzzthewiz@users.noreply.github.com>
2022-02-08 17:24:45 -0500

    added about and links and all that

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 function loadNav(id){
     const htmlString = `
-        <img src="./img/logo.png" onclick="location.href = './'">
-        <a href="./" id="home">Home</a>
-        <a href="./about" id="about">About</a>
+        <img src="https://izzzzthewiz.github.io/usm/img/logo.png" onclick="location.href = 'https://izzzzthewiz.github.io/usm/'">
+        <a href="https://izzzzthewiz.github.io/usm/" id="home">Home</a>
+        <a href="https://izzzzthewiz.github.io/usm/about" id="about">About</a>
         <div class="dropdown">
             <button id="links" onclick="location.href = '#links'">Links<i class="fas fa-caret-down"></i></button>
             <div class="dropdown-content">
-                <a href="./discord" target="_blank" id="discord">Discord</a>
-                <a href="./roster" target="_blank" id="roster">Roster</a>
-                <a href="./sop" target="_blank" id="sop">SOP</a>
-                <a href="./apply" target="_blank" id="apply">Apply</a>
-                <a href="./report" target="_blank" id="report">Report Form</a>
+                <a href="https://izzzzthewiz.github.io/usm/discord" target="_blank" id="discord">Discord</a>
+                <a href="https://izzzzthewiz.github.io/usm/roster" target="_blank" id="roster">Roster</a>
+                <a href="https://izzzzthewiz.github.io/usm/sop" target="_blank" id="sop">SOP</a>
+                <a href="https://izzzzthewiz.github.io/usm/apply" target="_blank" id="apply">Apply</a>
+                <a href="https://izzzzthewiz.github.io/usm/report" target="_blank" id="report">Report Form</a>
             </div>
         </div>
     `
@@ -20,11 +20,17 @@ function loadNav(id){
 function loadFooter(){
     const htmlString = `
     <p>Copyright &copy; <script>document.write(new Date().getFullYear())</script> United States Miltia</p>
-    <a href="./discord" target="_blank">Discord</a> | 
-    <a href="./roster" target="_blank">Roster</a> | 
-    <a href="./sop" target="_blank">SOP</a> | 
-    <a href="./apply" target="_blank">Apply</a> | 
-    <a href="./report" target="_blank">Report Form</a>
+    <a href="https://izzzzthewiz.github.io/usm/discord" target="_blank">Discord</a> | 
+    <a href="https://izzzzthewiz.github.io/usm/roster" target="_blank">Roster</a> | 
+    <a href="https://izzzzthewiz.github.io/usm/sop" target="_blank">SOP</a> | 
+    <a href="https://izzzzthewiz.github.io/usm/apply" target="_blank">Apply</a> | 
+    <a href="https://izzzzthewiz.github.io/usm/report" target="_blank">Report Form</a>
     `
     document.getElementById('footer').innerHTML = htmlString
+}
+function redirect(url){
+    const link = document.getElementById('link')
+    link.setAttribute('href', url)
+    link.innerHTML = url
+    location.href = url
 }
