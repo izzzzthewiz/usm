57e8b188bc55589c77dc425602a7ddf0eb3463bc
_iz_ <62268447+izzzzthewiz@users.noreply.github.com>
2022-02-08 23:27:29 -0500

    rotate arrow

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ function loadNav(id){
         <img src="https://izzzzthewiz.github.io/usm/img/logo.png" onclick="location.href = 'https://izzzzthewiz.github.io/usm/'">
         <a href="https://izzzzthewiz.github.io/usm/" id="home">Home</a>
         <a href="https://izzzzthewiz.github.io/usm/about" id="about">About</a>
-        <div class="dropdown">
-            <button id="links" onclick="location.href = '#links'">Links<i class="fas fa-caret-down"></i></button>
+        <div class="dropdown" id="dropdown">
+            <button id="links" onclick="location.href = '#links'">Links<i class="fas fa-caret-right" id="arrow"></i></button>
             <div class="dropdown-content">
                 <a href="https://izzzzthewiz.github.io/usm/discord" target="_blank" id="discord">Discord</a>
                 <a href="https://izzzzthewiz.github.io/usm/roster" target="_blank" id="roster">Roster</a>
@@ -17,6 +17,10 @@ function loadNav(id){
     document.getElementById('navcontent').innerHTML = htmlString
     document.getElementById(id).classList.add('active')
 }
+
+
+
+
 function loadFooter(){
     const htmlString = `
     <p>Copyright &copy; <script>document.write(new Date().getFullYear())</script> United States Miltia</p>
@@ -28,6 +32,9 @@ function loadFooter(){
     `
     document.getElementById('footer').innerHTML = htmlString
 }
+
+
+
 function redirect(url){
     const link = document.getElementById('link')
     link.setAttribute('href', url)
