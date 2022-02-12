91b211d1df8b5b0fa0973feb52fb78207736587a
_iz_ <62268447+izzzzthewiz@users.noreply.github.com>
2022-02-12 12:25:07 -0500

    Update content.js

--- content/content.js
+++ content/content.js
@@ -15,9 +15,7 @@ const displayVideos = (videos) => {
     const contentwrap = document.getElementById('contentwrap')
     const htmlString = videos
     .map((video) => {
-        let embed
-        if (video.platform == 'youtube') embed = `<iframe width="640" height="360" src="https://www.youtube.com/embed/${video.url.replace('https://www.youtube.com/watch?v=', '')}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
-        if (video.platform == 'medal') embed = `<iframe width='640' height='360' style='border: none;' src='${video.url}' allow='autoplay' allowfullscreen></iframe>`
+        const embed = `<iframe width='640' height='360' style='border: none;' src='${video.url}' allow='autoplay' allowfullscreen></iframe>`
         return `
         <div class="box lightred blue-border">
             <div class="center">
