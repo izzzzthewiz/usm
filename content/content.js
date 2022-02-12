a579cf311eb710167c6eb04888ccafb44c9ab897
_iz_ <62268447+izzzzthewiz@users.noreply.github.com>
2022-02-12 17:13:55 -0500

    added searchbar to content

--- content/content.js
+++ content/content.js
@@ -1,8 +1,14 @@
+const api = 'https://raw.githubusercontent.com/izzzzthewiz/usm/main/content/generator/api/videos.json'
 
+const searchbar = document.getElementById('searchbar')
+const contentwrap = document.getElementById('contentwrap')
+const searchbtn = document.getElementById('searchbtn')
+const clearbtn = document.getElementById('clearbtn')
+const cleardiv = document.querySelector('.searchclear')
 
 const loadVideos = async () => {
     try {
-        const res = await fetch('https://raw.githubusercontent.com/izzzzthewiz/usm/main/content/generator/api/videos.json')
+        const res = await fetch(api)
         let videos = await res.json()
         displayVideos(videos)
         console.log(videos)
@@ -12,7 +18,6 @@ const loadVideos = async () => {
 }
 
 const displayVideos = (videos) => {
-    const contentwrap = document.getElementById('contentwrap')
     const htmlString = videos
     .map((video) => {
         return `
@@ -32,5 +37,45 @@ const displayVideos = (videos) => {
     .join('')
     contentwrap.innerHTML = htmlString
 }
+
+async function search(searchString) {
+    if (searchbar.value == '') return clearSearch()
+    try {
+        const res = await fetch(api);
+        let videos = await res.json();
+        const filteredVideos = videos.filter((video) => {
+            return (
+                video.title.toLowerCase().includes(searchString) || video.creator.toLowerCase().includes(searchString)
+            );
+        });
+        displayVideos(filteredVideos);
+        if (filteredVideos.toString() == ''){
+            contentwrap.innerHTML = `<h1 class="center red-text" id="noresults">No videos found!</h1>`;
+        }
+        cleardiv.classList.add('show')
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+searchbar.addEventListener('keydown', (e) => {
+    if (e.keyCode == 13){
+        search(e.target.value)
+    }
+});
+
+searchbtn.addEventListener('click', () => {
+    search(searchbar.value)
+})
+
+function clearSearch(){
+    loadVideos()
+    searchbar.value = ''
+    cleardiv.classList.remove('show')
+}
+
+clearbtn.addEventListener('click', () => {
+    clearSearch()
+})
 
 loadVideos()
